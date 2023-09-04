aa7321af0a5f65aad6132da8d9ac5c49e68c3756
Owolabiadesola <127605911+Owolabiadesola@users.noreply.github.com>
2023-09-04 15:53:34 +0100

    Weather__App

--- src/Components/WeatherApp.js
+++ src/Components/WeatherApp.js
@@ -1,93 +1,3 @@
-// import React, { useState } from "react";
-// import { Container, Row, Col } from "react-bootstrap";
-
-// const api = {
-//   key: "6b52701097d9e87ac66a6b7b91660fc4",
-//   base: "https://api.openweathermap.org/data/2.5/",
-// };
-// const WeatherApp = () => {
-//   // const search = () => {
-//   //   const element = document.getElementsByClassName("cityInput");
-//   //   if (element[0].value === "") {
-//   //     return 0;
-//   //   }
-//   //  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=6b52701097d9e87ac66a6b7b91660fc4"
-
-//   const [search, setSearch] = useState("");
-
-//   const searchPressed = () => {
-//     console.log("Search pressed!");
-//     console.log(search);
-//   };
-
-//   return (
-//     <Container>
-//       <Row>
-//         <Col>
-//           <div className="main">
-//             <div className="top-bar">
-//               {/* Search Box */}
-
-//               <input
-//                 type="text"
-//                 className="cityInput"
-//                 placeholder="Search..."
-//                 onChange={(e) => setSearch(e.target.value)}
-//               />
-//               {/* <button onClick={(e) => searchPressed}>Search</button> */}
-//               <div className="search-icon">
-//                 <img
-//                   src="/Images/search1.png"
-//                   alt="img"
-//                   className="img-fluid search"
-//                   onClick={(e) => searchPressed}
-//                 />
-//               </div>
-//             </div>
-//             {/* Location */}
-//             <p className="weatherlocation mt-5">New York City, USA</p>
-//             {/* Temperature F/C */}
-//             <p className="weathertemp">32°F</p>
-//             {/* Condition (Sunny) */}
-//             <p className="weathercondition">Sunny</p>
-
-//             {/* <div className="weather-image">
-//               <img
-//                 src="/Images/cloudyyy-rm.png"
-//                 alt="img"
-//                 className="img-fluid cloudy"
-//               />
-//             </div>
-//             <div className="weathertemp">24°c</div>
-//             <div className="weatherlocation">London</div>
-//             <div className="datacontainer">
-//               <div className="element">
-//                 <img
-//                   src="Images/humidity1.png"
-//                   alt="img"
-//                   className="humidityicon"
-//                 />
-//                 <div className="data">
-//                   <div className="humiditypercent">64%</div>
-//                   <div className="text">Humidity</div>
-//                 </div>
-//               </div>
-//               <div className="element">
-//                 <img src="Images/windy.png" alt="img" className="windicon" />
-//                 <div className="data">
-//                   <div className="humiditypercent">18 km/h</div>
-//                   <div className="text">Wind Speed</div>
-//                 </div>
-//               </div>
-//             </div> */}
-//           </div>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// };
-
-// export default WeatherApp;
 import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
@@ -98,6 +8,7 @@ const api = {
 
 const WeatherApp = () => {
   const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
 
   const handleSearch = () => {
     if (search === "") {
@@ -105,16 +16,14 @@ const WeatherApp = () => {
       return;
     }
 
-  //  Perform the API request using `search` and `api` values
+    //  Perform the API request using `search` and `api` values
     //For example:
     const apiUrl = `${api.base}weather?q=${search}&appid=${api.key}`;
     fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((result) => {
         // Handle the data and update the UI
-      })
-      .catch(error => {
-        console.error("An error occurred:", error);
+        setWeather(result);
       });
   };
 
@@ -122,7 +31,7 @@ const WeatherApp = () => {
     <Container>
       <Row>
         <Col>
-          <div className="main">
+          <div className="main mt-3">
             <div className="top-bar">
               {/* Search Box */}
               <input
@@ -132,14 +41,22 @@ const WeatherApp = () => {
                 value={search}
                 onChange={(e) => setSearch(e.target.value)}
               />
-              <button onClick={handleSearch}>Search</button>
+              <div className="search-icon">
+                <img
+                  src="/Images/search1.png"
+                  alt="img"
+                  className="img-fluid search"
+                  onClick={handleSearch}
+                />
+              </div>
             </div>
             {/* Location */}
-            <p className="weatherlocation mt-5">New York City, USA</p>
+            <p className="weatherlocation mt-5">{weather.name}</p>
             {/* Temperature F/C */}
-            <p className="weathertemp">32°F</p>
+            <p className="weathertemp">{weather.main.temp}°F</p>
             {/* Condition (Sunny) */}
-            <p className="weathercondition">Sunny</p>
+            <p className="weathercondition">{weather.weather[0].main}</p>
+            <p className="weathercondition">{weather.weather[0].description}</p>
           </div>
         </Col>
       </Row>
